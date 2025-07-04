2a1324ffb850c5609dcea68c0f57baaf05688fb2
uwasejeannine <jeannineuwasee@gmail.com>
2025-07-04 16:09:11 +0200

    Include config directory for deployment

---
+++ backend/config/config.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv");
+
+// Configure dotenv to load environment variables
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    // Remove SSL for local development
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "POSTGRES_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+
+  // config email
+  EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+
+  JWT_SECRET: process.env.JWT_SECRET,
+};
